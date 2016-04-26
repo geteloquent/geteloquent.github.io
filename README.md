bed38e6197e731f0d7ef8cc0a2f2d35c6c3889f8
Juliana Lucena <julianalucenaa@gmail.com>
2016-04-26 17:32:22 -0300

    Site updated to 289929c

--- README.md
+++
@@ -1,4 +0,0 @@
-geteloquent.github.io
-=====================
-
-[![Stories in Ready](https://badge.waffle.io/geteloquent/geteloquent.github.io.png?label=ready&title=Ready)](http://waffle.io/geteloquent/geteloquent.github.io)
--- lib/custom_helpers.rb
+++
@@ -1,9 +0,0 @@
-module CustomHelpers
-  def navigation_pages
-    sitemap.where(primary_navigation: true).order_by(:weight).all
-  end
-
-  def slugify(term)
-    term.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-  end
-end
