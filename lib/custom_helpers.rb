769821b5dc84cfacd2d3416d77fbee242ffb4d2a
Juliana Lucena <julianalucenaa@gmail.com>
2016-05-03 13:00:39 -0300

    Avoids anchor duplication at navigation.

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -1,6 +1,7 @@
 module CustomHelpers
   def navigation_pages
-    sitemap.where(primary_navigation: true).order_by(:weight).all
+    nav_items = sitemap.where(primary_navigation: true).order_by(:weight).all
+    nav_items.select { |nav| nav.metadata[:locals][:lang] == I18n.locale }
   end
 
   def slugify(term)
